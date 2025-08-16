82745cbc54ccd0fd5bff9de0da6f003a246f4d7a
Collins Opiayo <opiayocollins2@gmail.com>
2025-08-16 18:56:19 +0300

    code challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,105 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
-        
+
+        author._articles.append(self)
+        magazine._articles.append(self)
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if hasattr(self, "_title"):
+            return
+        if not isinstance(value, str):
+            raise Exception("Title must be a string")
+        if not (5 <= len(value) <= 50):
+            raise Exception("Title must be between 5 and 50 characters")
+        self._title = value
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        if not isinstance(value, Author):
+            raise Exception("Author must be an Author instance")
+        self._author = value
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, value):
+        if not isinstance(value, Magazine):
+            raise Exception("Magazine must be a Magazine instance")
+        self._magazine = value
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str) or len(name) == 0:
+            raise Exception("Author name must be a non-empty string")
+        self._name = name
+        self._articles = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        raise AttributeError("Author name is immutable")
 
     def articles(self):
-        pass
+        return self._articles
 
     def magazines(self):
-        pass
+        return list({article.magazine for article in self._articles})
 
     def add_article(self, magazine, title):
-        pass
+        article = Article(self, magazine, title)
+        self._articles.append(article)
+        magazine._articles.append(article)
+        return article
 
     def topic_areas(self):
-        pass
-
+        if not self._articles:
+            return None
+        return list({article.magazine.category for article in self._articles})
 class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        self._articles = []
 
-    def articles(self):
-        pass
+    @property
+    def name(self):
+        return self._name
 
-    def contributors(self):
-        pass
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise Exception("Magazine name must be a string")
+        if not (2 <= len(value) <= 16):
+            raise Exception("Magazine name must be between 2 and 16 characters")
+        self._name = value
 
-    def article_titles(self):
-        pass
+    @property
+    def category(self):
+        return self._category
 
-    def contributing_authors(self):
-        pass
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str):
+            raise Exception("Magazine category must be a string")
+        if len(value) < 3:
+            raise Exception("Magazine category must be at least 3 characters")
+        self._category = value
